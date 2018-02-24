8a947636c7508ef8d20e7098bdb990702c22d2e5
Tushar Sharma <tsharma@syncsort.com>
2018-02-24 12:58:55 +0000

    modularity added

--- src/main.py
+++ src/main.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+#  Decription
+#  -----------
+#  * Backup secrets from www.protectedtext.com to local storage
+#  * Decryption: `base64 -d BACKUP_FILE | openssl aes-256-cbc -d -k PASSWORD`
 
 import urllib.request
 import json
@@ -8,44 +12,61 @@ import datetime
 import logging
 import sys
 
-PATH = os.getcwd()
-log_file = PATH + '/logs/events.log'
-backup_file = PATH + "/backup/"
-
-logging.basicConfig(filename=log_file,
-                    level = logging.DEBUG,
-                    format='%(asctime)s %(levelname)s %(message)s')
-
 def log_except_hook(*exc_info):
     exc = "".join(tracebook.format_exception(*exc_info))
     logging.critical(exc)
     failure(exc)
 
-sys.excepthook = log_except_hook
-logging.debug("Start")
-
-sites = ["golmal"]
-
+"""
+Get encrypted data from protectedtext.com
+"""
 def get_content(site):
     req = urllib.request.urlopen("https://www.protectedtext.com/%s?action=getJSON" % site)
     return json.loads(req.read().decode("utf-8"))['eContent']
 
-def save_file(data, site):
-    secret_file = PATH + "/backup/" + site
+"""
+Save data to disk
+"""
+def save_file(data, site, backup_file):
     with open(secret_file, "w") as f:
         f.write(data)
         logging.debug("Wrote: " + site)
 
+"""
+create directories if not exist
+"""
+def setup():
+    if not os.path.exists(PATH):
+        os.makedirs(PATH)
+
 def main():
+    PATH = os.getcwd()
+    log_path = PATH + '/logs/'
+    backup_path = PATH + "/backup/"
+    log_file = log_path + 'events.log'
+
+    sys.excepthook = log_except_hook
+    logging.debug("Start")
+
+    sites = ["golmal"]
+
+    logging.basicConfig(filename=log_path + 'events.log',
+                    level = logging.DEBUG,
+                    format='%(asctime)s %(levelname)s %(message)s')
+
+
+    setup(log_path)
+    setup(lbackup_path)
 
     for site in sites:
+        backup_file = backup_path + site
         content = get_content(site)
 
         if not content:
             logging.debug("There is no secret "  + site )
-            sys.exit(2)
+            sys.exit(1)
 
-        save_file(content, site)
+        save_file(content, site, backup_file)
 
 
 if __name__=="__main__":
